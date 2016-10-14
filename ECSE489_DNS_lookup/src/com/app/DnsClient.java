0e118714afe26e28c3f627ae3991fbe4f4662ca0
ch-andrei <im.andrei@hotmail.ca>
2016-10-14 17:45:31 -0400

    removed hashtable from dnsclient

--- ECSE489_DNS_lookup/src/com/app/DnsClient.java
+++ ECSE489_DNS_lookup/src/com/app/DnsClient.java
@@ -1,7 +1,6 @@
 package com.app;
 
 import java.util.ArrayList;
-import java.util.Hashtable;
 import java.io.IOException;
 
 public class DnsClient {
@@ -10,19 +9,14 @@ public class DnsClient {
     private static final String DEFAULT_MAX_RETRIES = "3";
     private static final String DEFAULT_PORT = "53";
 
-    public static ArrayList<String> cmdSplit(String[] args) throws IOException {
-
-        Hashtable<String, String> hashTable = new Hashtable<String, String>();
+    public static ArrayList<String> split_cmd_args(String[] args) throws IOException
+    {
         String flag = "A";
         String server_ip = "";
         String domain_name = "";
 
-        hashTable.put("-t", DEFAULT_TIMEOUT);
-        hashTable.put("-r", DEFAULT_MAX_RETRIES);
-        hashTable.put("-p", DEFAULT_PORT);
-
-        if (args.length == 0) {
-            throw new IOException("Dns Client is missing all arguments");
+        if (args.length < 3) {
+            throw new IOException("DNS Client missing arguments!");
         }
 
         for (int i = 0; i < args.length; i++) {
@@ -46,9 +40,9 @@ public class DnsClient {
         }
 
         ArrayList<String> command = new ArrayList<>();
-        command.add(0, hashTable.get("-t"));
-        command.add(1, hashTable.get("-r"));
-        command.add(2, hashTable.get("-p"));
+        command.add(0, DEFAULT_TIMEOUT);
+        command.add(1, DEFAULT_MAX_RETRIES);
+        command.add(2, DEFAULT_PORT);
         command.add(3, flag);
         command.add(4, server_ip);
         command.add(5, domain_name);
@@ -58,6 +52,15 @@ public class DnsClient {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
+        ArrayList lookup_args;
+        try {
+            lookup_args = split_cmd_args(args);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
 
     }
 }
